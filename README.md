e24861232de4573b46a425e03a803da88276bde2
Clinton <135718431+clix-M@users.noreply.github.com>
2023-10-17 09:11:57 -0500

    .
    .

--- README.md
+++ README.md
@@ -1,58 +1,33 @@
-# javaSwingForm  EmployeeSystemManagement
+# javaSwingForm
 
 ## Description
-Este proyecto es un ejemplo de como crear un Systema de Control de Empleados con Java Swing y MySQL.
-
+Este proyecto es un ejemplo de como crear un formulario con Java Swing.
 
 ## Proyecto
- ![Vista](/src/img/1_s.png)
- ![Succes](/src/img/2.png)
- ![Error](/src/img/3_s.png)
- ![Mysql](/src/img/4_s.png)
-![Mysql](/src/img/6_s.png)
-
-## MySQL
-![Mysql](/src/img/0_SQL.png)
-![Mysql](/src/img/1_SQL.png)
-![Mysql](/src/img/3_sql%20gr.png)
+ ![Vista](/src/img/1_login.png)
+ ![Succes](/src/img/2_loginsucces.png)
+ ![Error](/src/img/3_error.png)
+ ![Mysql](/src/img/5_db.png)
 
 ## Instalación
 Para poder ejecutar el proyecto es necesario tener instalado:
 * Java 20 -> https://www.oracle.com/java/technologies/javase-jdk20-downloads.html
 * MySQL 8 -> https://railway.app/project/27b7ddaf-464f-48b5-9d80-dd9fee7976f8/plugin/0c8c491b-ddd1-4f0f-804b-7738e786cfac/data?state=table&table=users
-    # importar
+    
 * mysql-connector-java-8.0.22.jar
 
 ## Query
 ```sql
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 
-
-CREATE TABLE `cargo` (
-  `ID_CARGO` int PRIMARY KEY NOT NULL AUTO_INCREMENT ,
-  `NOMBRECARGO` varchar(200),
-  `ESTADO` bit(1)
 );
-
-CREATE TABLE `empleado` (
-  `ID_EMPLEADO` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `NOMBRE` varchar(200),
-  `APELLIDOS` varchar(200),
-  `SEXO` char(1),
-  `TELEFONO` char(12),
-  `FECHANACIMIENTO` date,
-  `TIPDOCUMENTO` char(11),
-  `ID_USUARIO` int
-);
-
-CREATE TABLE `usuario` (
-  `ID_USUARIO` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `NOMBREUSUARIO` varchar(200),
-  `CLAVE` varchar(10),
-  `ESTADO` bit(1),
-  `ID_CARGO` int
-);
-
-ALTER TABLE `empleado` ADD FOREIGN KEY (`ID_USUARIO`) REFERENCES `usuario` (`ID_USUARIO`);
-
-ALTER TABLE `usuario` ADD FOREIGN KEY (`ID_CARGO`) REFERENCES `cargo` (`ID_CARGO`);
 ```
+
+
+
+
+
--- src/main/java/org/example/LoginRegistrationForm.java
+++ src/main/java/org/example/LoginRegistrationForm.java
@@ -1,26 +1,20 @@
 package org.example;
 
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-
 public class LoginRegistrationForm extends JFrame implements ActionListener {
-
-   // mysql://root:MK9hQNvAibnbFC7rWbg8@containers-us-west-89.railway.app:5970/railway
     // Global variables
-    private static final String URL = "jdbc:mysql://containers-us-west-89.railway.app:5970/railway";
+    private static final String URL = "jdbc:mysql://containers-us-west-81.railway.app:6154/railway";
     private static final String USER = "root";
-    private static final String PASSWORD = "MK9hQNvAibnbFC7rWbg8";
-
+    private static final String PASSWORD = "9kFumuSs26Xy8oU8B34l";
     private static final int PANEL_WIDTH = 400;
     private static final int PANEL_HEIGHT = 400;
 
     // Components
     private JPanel panel;
-    private JLabel titleLabel, nombreLabel, passwordLabel, confirmPasswordLabel;
+    private JLabel titleLabel, emailLabel, passwordLabel, confirmPasswordLabel;
     private JTextField emailField;
     private JPasswordField passwordField, confirmPasswordField;
     private JButton loginButton, registerButton;
@@ -47,46 +41,46 @@ public class LoginRegistrationForm extends JFrame implements ActionListener {
         panel.add(titleLabel);
 
         // Set up the email label and field
-        nombreLabel = new JLabel("Nombre");
-        nombreLabel.setBounds(50, 100, 100, 30);
-        panel.add(nombreLabel);
+        emailLabel = new JLabel("Email:");
+        emailLabel.setBounds(50, 100, 100, 30);
+        panel.add(emailLabel);
         emailField = new JTextField();
-        emailField.setBounds(174, 100, 176, 30);
+        emailField.setBounds(150, 100, 200, 30);
         panel.add(emailField);
 
         // Set up the password label and field
-        passwordLabel = new JLabel("Clave:");
+        passwordLabel = new JLabel("Password:");
         passwordLabel.setBounds(50, 150, 100, 30);
 
 
         panel.add(passwordLabel);
         passwordField = new JPasswordField();
-        passwordField.setBounds(174, 150, 176, 30);
+        passwordField.setBounds(150, 150, 200, 30);
         panel.add(passwordField);
 
         // Set up the confirm password label and field
-        confirmPasswordLabel = new JLabel("Confirmar Clave:");
+        confirmPasswordLabel = new JLabel("Confirm Password:");
         confirmPasswordLabel.setBounds(50, 200, 100, 30);
         panel.add(confirmPasswordLabel);
         confirmPasswordField = new JPasswordField();
-        confirmPasswordField.setBounds(174, 200, 176, 30);
+        confirmPasswordField.setBounds(150, 200, 200, 30);
         panel.add(confirmPasswordField);
 
         // Set up the login and register buttons
-        loginButton = new JButton("Inisiar Sesión");
-        loginButton.setBounds(222, 291, 128, 30);
+        loginButton = new JButton("Login");
+        loginButton.setBounds(50, 250, 100, 30);
         loginButton.setBackground(Color.decode("#00b894"));
 
         loginButton.addActionListener(this);
         panel.add(loginButton);
-        registerButton = new JButton("Registrarse");
-        registerButton.setBounds(62, 291, 117, 30);
+        registerButton = new JButton("Register");
+        registerButton.setBounds(250, 250, 100, 30);
         registerButton.setBackground(Color.decode("#00b894"));
         registerButton.addActionListener(this);
         panel.add(registerButton);
 
         // Set up the frame
-        getContentPane().add(panel);
+        add(panel);
         pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -99,68 +93,67 @@ public class LoginRegistrationForm extends JFrame implements ActionListener {
             // Validate the credentials of the user and show an error alert if they are incorrect
 
             if (emailField.getText().equals("") || passwordField.getPassword().length == 0) {
-                JOptionPane.showMessageDialog(panel, "Please enter an name and password.");
+                JOptionPane.showMessageDialog(panel, "Please enter an email and password.");
                 return;
             }
 
-            String username = emailField.getText();
+            String email = emailField.getText();
             String password = new String(passwordField.getPassword());
 
-            if (username.equals("") || password.equals("")) {
-                JOptionPane.showMessageDialog(panel, "Please enter an name and password.");
+            if (email.equals("") || password.equals("")) {
+                JOptionPane.showMessageDialog(panel, "Please enter an email and password.");
                 return;
             }
-
-            // si el usuario y la contraseña son correctos y ID_CARGO = 1 -> admin ABRIR EmployeeManagementSystem
-
 
             try {
                 Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM usuario WHERE NOMBREUSUARIO='" + username + "' AND CLAVE='" + password + "'");
+                ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE email='" + email + "' AND password='" + password + "'");
                 if (rs.next()) {
-                    //JOptionPane.showMessageDialog(panel, "Login successful!");
-                    //System.out.println("Login successful!");
-                    //System.out.println(rs.getInt("ID_CARGO"));
-                    if (rs.getInt("ID_CARGO") == 1) {
-                        //System.out.println("admin");
-                        // abrir EmployeeManagementSystem
-                        EmployeeManagementSystem employeeManagementSystem = new EmployeeManagementSystem();
-                        employeeManagementSystem.setVisible(true);
-                        employeeManagementSystem.pack();
-                        employeeManagementSystem.setLocationRelativeTo(null);
-                        employeeManagementSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        this.dispose();
-                    } else {
-                        //System.out.println("empleado");
-                        Employee  employee = new Employee();
-                        employee.setVisible(true);
-                        employee.pack();
-                        employee.setLocationRelativeTo(null);
-                        employee.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                        this.dispose();
-                    }
+                    JOptionPane.showMessageDialog(panel, "Login successful!");
                 } else {
                     JOptionPane.showMessageDialog(panel, "Invalid email or password.");
                 }
+                conn.close();
+                // al cerrar que el color de fondo sea normal
 
-                // conn.close();
+
+
+
+
+
+
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
-
-
         } else if (e.getSource() == registerButton) {
             // Validate the data of the user and create a new account in the database
+            String email = emailField.getText();
+            String password = new String(passwordField.getPassword());
+            String confirmPassword = new String(confirmPasswordField.getPassword());
+            if (!password.equals(confirmPassword)) {
+                JOptionPane.showMessageDialog(panel, "Passwords do not match.");
+            } else {
+                try {
+                    if (email.equals("") || password.equals("")) {
+                        JOptionPane.showMessageDialog(panel, "Please enter an email and password.");
+                        return;
+                    }
 
-            // is a la clase AddEmployeeForm
-            RegistroForm addEmployeeForm = new RegistroForm();
-            addEmployeeForm.setVisible(true);
-            addEmployeeForm.pack();
-            addEmployeeForm.setLocationRelativeTo(null);
-            addEmployeeForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.dispose();
-
+                    Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE email='" + email + "'");
+                    if (rs.next()) {
+                        JOptionPane.showMessageDialog(panel, "Email already exists.");
+                    } else {
+                        stmt.executeUpdate("INSERT INTO users (email, password) VALUES ('" + email + "', '" + password + "')");
+                        JOptionPane.showMessageDialog(panel, "Registration successful!");
+                    }
+                    conn.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 
